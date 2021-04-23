659aa48b87da3c684255f5c4239cf277e1157cdb
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-04-23 16:29:42 +0300

    initial commit

--- Advanced/Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ Advanced/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Fast_Food
 {
@@ -6,7 +8,34 @@ namespace _04._Fast_Food
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int quantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>(orders);
+
+            Console.WriteLine(queue.Max());
+
+            bool isCompleted = true;
+            for (int i = 0; i < orders.Length; i++)
+            {
+                if (orders[i] <= quantity)
+                {
+                   quantity-= queue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ",queue)}");
+                    isCompleted = false;
+                    break;
+                    
+                }
+            }
+            if (isCompleted)
+            {
+                Console.WriteLine("Orders complete");
+            }
+
         }
     }
 }
