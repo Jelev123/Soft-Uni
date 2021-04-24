bdb25ebd35377a1826d45bd3aa0e62e0fffbfd50
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-04-24 16:29:05 +0300

    initial commit

---
+++ Advanced/Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sized = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = sized[0];
+            var cols = sized[1];
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var col = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int c = 0; c < cols; c++)
+                {
+                    matrix[row, c] = col[c];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
