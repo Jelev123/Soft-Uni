24926dc414b046ac33ea20e05dcd7295635ace54
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-04-24 15:19:10 +0300

    initial commit

--- Advanced/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
+++ Advanced/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _07._Truck_Tour
 {
@@ -6,7 +7,31 @@ namespace _07._Truck_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] pumps = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pumps[i] = integers[0] - integers[1];
+            }
+
+            int current = 0;
+            int position = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                current += pumps[i];
+
+                if (current < 0)
+                {
+                    current = 0;
+                    position = i + 1;
+                }
+            }
+            Console.WriteLine(position);
         }
     }
-}
+    }
+
