a296039f8664bcf534a392babbdb7acd6fca858f
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-04-27 15:11:15 +0300

    initial commit

--- Advanced/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
+++ Advanced/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1._Diagonal_Difference
 {
@@ -6,7 +7,36 @@ namespace _1._Diagonal_Difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            var jaged = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                jaged[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            int firstDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                firstDiagonal += jaged[row][row];
+                
+            }
+
+            int secondDiagonal = 0;
+
+           
+
+            for (int row = 0,col=n-1; row < n; row++,col--)
+            {
+                secondDiagonal += jaged[row][col];
+            }
+
+            Console.WriteLine(secondDiagonal-firstDiagonal);
         }
     }
 }
