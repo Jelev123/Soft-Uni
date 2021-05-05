1508f635c1842ef6f8caf6c2e53149855b3876a6
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-05 18:07:02 +0300

    initial commit

--- Advanced/Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
+++ Advanced/Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Linq;
 
 namespace _6._Jagged_Array_Manipulator
@@ -7,7 +8,9 @@ namespace _6._Jagged_Array_Manipulator
     {
         static void Main(string[] args)
         {
+
             int n = int.Parse(Console.ReadLine());
+
             var jagged = new double[n][];
 
             for (int i = 0; i < n; i++)
@@ -17,17 +20,55 @@ namespace _6._Jagged_Array_Manipulator
                     .ToArray();
             }
 
-            Analyze(jagged);
+            Checked(jagged);
+            Command(jagged);
 
-            Cheked(jagged);
 
         }
 
-        private static void Analyze(double[][] jagged)
+        private static void Command(double[][] jagged)
         {
-            for (int row = 0; row < jagged.Length-1; row++)
+            string input = Console.ReadLine();
+
+            while (input != "End")
             {
-                if (jagged[row].Length == jagged[row+1].Length)
+                string[] splited = input.Split();
+
+                string command = splited[0];
+                int row = int.Parse(splited[1]);
+                int col = int.Parse(splited[2]);
+                int value = int.Parse(splited[3]);
+
+
+
+                if (row >= 0 && row < jagged.Length && col >= 0 && col < jagged[row].Length)
+                {
+                    if (command == "Add")
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else
+                    {
+                        jagged[row][col] -= value;
+                    }
+
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var i in jagged)
+            {
+                Console.WriteLine(string.Join(" ", i));
+            }
+        }
+
+        private static void Checked(double[][] jagged)
+        {
+            for (int row = 0; row < jagged.Length - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
                 {
                     for (int col = 0; col < jagged[row].Length; col++)
                     {
@@ -44,45 +85,12 @@ namespace _6._Jagged_Array_Manipulator
 
                     for (int col = 0; col < jagged[row + 1].Length; col++)
                     {
-                        jagged[row+1][col] /= 2;
+                        jagged[row + 1][col] /= 2;
                     }
+
+
                 }
             }
-        }
-
-        private static void Cheked(double[][] jagged)
-        {
-            string input = Console.ReadLine();
-
-            while (input != "End")
-            {
-                string[] splited = input.Split();
-                string command = splited[0];
-                int row = int.Parse(splited[1]);
-                int col = int.Parse(splited[2]);
-                int value = int.Parse(splited[3]);
-
-
-                if (row >= 0 && row < jagged.Length && col >= 0 && col < jagged[row].Length)
-                {
-                    if (command == "Add")
-                    {
-                        jagged[row][col] += value;
-                    }
-                    else
-                    {
-                        jagged[row][col] -= value;
-                    }
-                }
-                input = Console.ReadLine();
-
-            }
-
-            foreach (var item in jagged)
-            {
-                Console.WriteLine(string.Join(" ", item));
-            }
-
 
         }
     }
