a7189f94869b39ff12705e4c6f6cd210ca7a31a0
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-12 10:48:53 +0300

    initial commit

---
+++ Advanced/Sets and Dictionaries Advanced - Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var parking = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input!= "END")
+            {
+                string[] cars = input.Split(", ");
+
+                string direction = cars[0];
+                string numberPlates = cars[1];
+
+                if (direction == "IN")
+                {
+                    parking.Add(numberPlates);
+                }
+
+                if (direction== "OUT")
+                {
+                    parking.Remove(numberPlates);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var cars in parking)
+            {
+                Console.WriteLine(cars);
+            }
+        }
+    }
+}
