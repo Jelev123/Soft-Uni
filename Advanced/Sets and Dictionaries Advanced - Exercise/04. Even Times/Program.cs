cdd8f064aad4d7ae00aaa5e0ba4bc97c71ebf218
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-12 12:15:47 +0300

    initial commit

---
+++ Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (input % 2 == 0 || input % 2 != 0)
+                {
+                    set.Add(input);
+                }
+            }
+
+            foreach (var i in set)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
