617d087a49c4944fb2ead93af124c1029760779b
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-12 11:10:33 +0300

    initial commit

--- Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
+++ Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Unique_Usernames
 {
@@ -6,7 +7,25 @@ namespace _01._Unique_Usernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string names = Console.ReadLine();
+
+                if (!set.Contains(names))
+                {
+                    set.Add(names);
+                }
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+            
         }
     }
 }
