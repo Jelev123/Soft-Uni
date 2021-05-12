f79ff31a0873942c3416a3c2092c2f238d5329be
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-12 11:42:38 +0300

    initial commit

---
+++ Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var element = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var n = element[0];
+            var m = element[1];
+            var nSet = new HashSet<int>();
+            var mSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                nSet.Add(input);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                mSet.Add(input);
+            }
+
+            nSet.SetEquals(mSet);
+
+            Console.WriteLine(string.Join(" ",nSet));
+        }
+    }
+}
