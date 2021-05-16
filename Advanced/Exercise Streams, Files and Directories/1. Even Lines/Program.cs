6f1879b20cabf88b372e972f02a90762edc39864
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-16 15:06:14 +0300

    initial commit

---
+++ Advanced/Exercise Streams, Files and Directories/1. Even Lines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _1._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("text.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                while (line != null)
+                {
+                    line = line.Replace(",", "@");
+                    line = line.Replace(".", "@");
+                    line = line.Replace("?", "@");
+                    line = line.Replace("!", "@");
+                    line = line.Replace("-", "@");
+                    
+
+                    if (counter % 2 == 0)
+                    {
+                        var array = line.Split().Reverse().ToArray();
+                        Console.WriteLine(string.Join(" ",array));
+                    }
+
+                    counter++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
