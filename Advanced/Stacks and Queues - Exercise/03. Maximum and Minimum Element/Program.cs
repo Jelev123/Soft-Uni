ef83b10e0db9982f850e0ededf33dfb5cb28d58c
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-16 12:43:41 +0300

    initial commit

--- Advanced/Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
+++ Advanced/Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -8,24 +8,22 @@ namespace _03._Maximum_and_Minimum_Element
     {
         static void Main(string[] args)
         {
-            
-
-            var stack = new Stack<int>();
 
             int n = int.Parse(Console.ReadLine());
-            int max = int.MaxValue;
-            int min = int.MinValue;
+            var stack = new Stack<int>();
+
+
             for (int i = 0; i < n; i++)
             {
                 int[] commandAsIndex = Console.ReadLine().Split()
-                    .Select(int.Parse).ToArray();
-
+                    .Select(int.Parse)
+                    .ToArray();
                 int command = commandAsIndex[0];
 
                 if (command == 1)
                 {
-                    int commandElements = commandAsIndex[1];
-                    stack.Push(commandElements);
+                    int elementsToPush = commandAsIndex[1];
+                    stack.Push(elementsToPush);
                 }
                 else if (command == 2)
                 {
@@ -42,6 +40,8 @@ namespace _03._Maximum_and_Minimum_Element
             }
 
             Console.WriteLine(string.Join(", ",stack));
+
+
         }
     }
 }
