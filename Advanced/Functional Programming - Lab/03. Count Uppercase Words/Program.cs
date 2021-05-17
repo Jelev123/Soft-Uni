58a52ba2c2ed7d9ef0f4e43660578c71663d4f20
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-17 11:20:51 +0300

    initial commit

---
+++ Advanced/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x[0] == x.ToUpper()[0])
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n",words));
+
+        }
+    }
+}
