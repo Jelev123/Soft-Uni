ce427f74f088cced7dc72eeb6ba57e1889e17fa9
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-17 13:45:35 +0300

    initial commit

---
+++ Advanced/Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Filter_By_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var person = new Dictionary<string, List<int>>();
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string conditional = Console.ReadLine();
+            string format = Console.ReadLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!person.ContainsKey(name))
+                {
+                    person.Add(name,new List<int>());
+                }
+
+                if (!person[name].Contains(age))
+                {
+                    person[name].Add(age);
+                }
+
+                person[name][age]++;
+                
+
+                if (conditional == "older")
+                {
+                    if (format == "name age")
+                    {
+                        if (age >= 20)
+                        {
+                            Console.WriteLine($"{name} - {age}");
+                        }
+                    }
+                }
+            }
+
+          
+        }
+    }
+}
