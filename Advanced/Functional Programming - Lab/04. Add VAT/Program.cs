cff575d94b14db9a6035f2b9c40bf4a4adc5b00d
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-17 11:53:34 +0300

    initial commit

---
+++ Advanced/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] price = Console.ReadLine()
+                .Split(", ")
+                .Select(decimal.Parse)
+                .Select(x=>x*1.2m)
+                .ToArray();
+
+            foreach (var vat in price)
+            {
+                Console.WriteLine(($"{vat:F2}"));
+            }
+            
+        }           
+    }
+}
