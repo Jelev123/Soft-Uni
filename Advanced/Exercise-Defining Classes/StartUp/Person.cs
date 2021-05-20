6bf0ffe336fb02fc226265d5492c7adde0a3563c
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-20 19:49:45 +0300

    initial commit

--- Advanced/Exercise-Defining Classes/StartUp/Person.cs
+++ Advanced/Exercise-Defining Classes/StartUp/Person.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DefiningClasses
 {
    public class Person
    {
-       private string name;
-       private int age;
-
+     
        public string Name { get; set; }
        public int Age { get; set; }
+
+
+       public Person()
+       {
+           Name = "No name";
+           Age = 1;
+       }
+
+       public Person(int age)
+      :this()
+       {
+           Age = age;
+       }
+
+       public Person(string name , int age) 
+           :this(age)
+       {
+           Name = name;
+           
+       }
+
+       public override string ToString()
+       {
+           return $"{this.Name} {this.Age}";
+       }
    }
 }
--- Advanced/Exercise-Defining Classes/StartUp/StartUp .cs
+++ Advanced/Exercise-Defining Classes/StartUp/StartUp .cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Define_a_Class_Person;
 
 namespace DefiningClasses
 {
@@ -6,11 +7,25 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "Pesho";
-            person.Age = 30;
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
 
-            Console.WriteLine($"{person.Name} {person.Age}");
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+            Person person = new Person(name,age);
+
+            family.AddMember(person);
+
+
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+
         }
     }
 }
