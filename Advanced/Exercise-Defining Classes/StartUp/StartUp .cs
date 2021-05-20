08391187f6785989f7db9aa7ad5f61a00ea0348f
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-20 15:44:36 +0300

    initial commit

---
+++ Advanced/Exercise-Defining Classes/StartUp/StartUp .cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Pesho";
+            person.Age = 30;
+
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
