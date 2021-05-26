9ca4d35b2fd97f9c7e210bd4a6775fea1b8aa5bd
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-26 17:10:32 +0300

    initial commit

---
+++ Advanced/Generics/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+   public class Box<T>
+    {
+        public Box(List<T> names)
+        {
+            Names = names;
+        }
+
+        public Box()
+        {
+            Names = new List<T>();
+        }
+
+        public List<T> Names { get; set; }
+
+
+        public void Add(T item)
+        {
+            Names.Add(item);
+        }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var name in Names)
+            {
+                sb.AppendLine($"{typeof(T)}: {name}");
+            }
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Advanced/Generics/GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                box.Add(line);
+            }
+
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var first = nums[0];
+            var second = nums[1];
+            Swap(box.Names,first,second);
+
+            Console.WriteLine(box);
+
+        }
+
+        static void Swap<T>(List<T>listWithData,int firstIndex,int secondIndex)
+        {
+            var temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
