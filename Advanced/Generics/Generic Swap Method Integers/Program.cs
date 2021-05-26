e4dc6cf1e598db791edd4b5661ca79d56658411d
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-26 17:16:08 +0300

    initial commit

--- Advanced/Generics/Generic Swap Method Integers/Program.cs
+++ Advanced/Generics/Generic Swap Method Integers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Generic_Swap_Method_Integers
 {
@@ -6,7 +8,32 @@ namespace Generic_Swap_Method_Integers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                box.Add(line);
+            }
+
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var first = nums[0];
+            var second = nums[1];
+            Swap(box.Names, first, second);
+
+            Console.WriteLine(box);
+
+        }
+
+        static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            var temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
         }
     }
-}
+    }
+
