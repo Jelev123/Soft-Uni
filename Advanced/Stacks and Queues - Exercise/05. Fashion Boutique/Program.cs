8e00295370f784e3d0aa9f96e7a27e8af3ee15f4
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-30 17:42:30 +0300

    initial commit

--- Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
+++ Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -8,7 +8,29 @@ namespace _05._Fashion_Boutique
     {
         static void Main(string[] args)
         {
-            
+            var clothes = Console.ReadLine().Split().Select(int.Parse);
+            int capacyOfRack = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>(clothes);
+            int sum = 0;
+            int numberOfRacks = 1;
+
+            while (stack.Count>0)
+            {
+                sum += stack.Peek();
+
+                if (sum <=capacyOfRack)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    numberOfRacks++;
+                  
+                }
+            }
+
+            Console.WriteLine(numberOfRacks);
         }
     }
 }
