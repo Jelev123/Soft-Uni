f281c8b8142445537db560ec056449ed13ebceef
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-30 14:29:48 +0300

    initial commit

---
+++ Advanced/Generics/Generic Box of Integer/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        public Box(int numbers)
+        {
+            Numbers = numbers;
+        }
+
+        public int Numbers { get; set; }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+            return $"{Numbers.GetType()}: {Numbers}";
+        }
+
+        #endregion
+    }
+}
