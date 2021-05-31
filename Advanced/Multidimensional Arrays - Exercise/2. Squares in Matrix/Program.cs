56fb24973dbad6689e6cab8bd9cc7ebf24d99e82
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-31 13:42:38 +0300

    initial commit

--- Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
+++ Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -7,33 +7,36 @@ namespace _2._Squares_in_Matrix
     {
         static void Main(string[] args)
         {
-            var dimension = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+            var dimension = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
                 .ToArray();
 
             int rows = dimension[0];
             int cols = dimension[1];
-            char[,] matrix = new char[rows,cols];
+            char[,] matrix = new char[rows, cols];
 
             InitializeMatrix(matrix);
 
             int counter = 0;
 
-            for (int row = 0; row < matrix.GetLength(0)-1; row++)
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
             {
-                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                 {
-                    if (matrix[row,col] == matrix[row,col +1] && 
-                        matrix[row,col] == matrix[row +1,col +1]&&
-                        matrix[row,col] == matrix[row + 1,col])
-                    {
-                        counter++;
-                    }
+                   
+                        if (matrix[row, col] == matrix[row, col + 1] &&
+                            matrix[row, col] == matrix[row + 1, col + 1] &&
+                            matrix[row, col] == matrix[row + 1, col])
+                        {
+                            counter++;
+                        }
+                    
+                    
                 }
             }
 
             Console.WriteLine(counter);
-          
-            
+
+
 
         }
 
@@ -52,8 +55,8 @@ namespace _2._Squares_in_Matrix
                 }
             }
 
-           
+
         }
     }
-    }
+}
 
