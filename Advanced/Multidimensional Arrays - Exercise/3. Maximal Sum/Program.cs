033ef5c862d83523a2b9df9a0036ffbe1854eac4
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-05-31 14:00:59 +0300

    initial commit

--- Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
+++ Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -20,20 +20,24 @@ namespace _3._Maximal_Sum
             int targetRow = 0;
             int targetCol = 0;
 
-            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
-                {
-                    if (row >= 0 && row < matrix.Length && col >= 0 && col < matrix.Length)
-                    {
-                        int currnetSum = matrix[row, col] + matrix[row + 1,
-                            col] + matrix[row + 2, col] + matrix[row, col + 1] + matrix[row, col + 2] +
-                            matrix[row + 1, col + 1] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2] +
-                            matrix[row + 1, col + 2];
 
-                        if (currnetSum > maxSum)
+            for (int row = 0; row < matrix.GetLength(0)-2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-2; col++)
+                {
+                    if (row >=0 && row<matrix.Length && col>= 0 && col < matrix.Length)
+                    {
+                        int currentSum = matrix[row, col] + matrix[row + 1, col]
+                                                           + matrix[row + 2, col]
+                                                           + matrix[row, col + 1]
+                                                           + matrix[row, col + 2]
+                                                           + matrix[row + 1, col + 1]
+                                                           + matrix[row + 2, col + 1]
+                                                           + matrix[row + 1, col + 2]
+                                                           + matrix[row + 2, col + 2];
+                        if (currentSum > maxSum)
                         {
-                            maxSum = currnetSum;
+                            maxSum = currentSum;
                             targetRow = row;
                             targetCol = col;
                         }
@@ -42,15 +46,18 @@ namespace _3._Maximal_Sum
             }
 
             Console.WriteLine($"Sum = {maxSum}");
-
-            for (int row = targetRow; row <= targetRow + 2; row++)
+            for (int i = targetRow; i <= targetRow+2; i++)
             {
-                for (int col = targetCol; col <= targetCol + 2; col++)
+                for (int j = targetCol; j <= targetCol+2; j++)
                 {
-                    Console.Write(matrix[row, col] + " ");
+                    Console.Write(matrix[i,j]+ " ");
                 }
+
                 Console.WriteLine();
             }
+            
+
+            
         }
 
         private static void InitializeMatrix(int[,] matrix)
