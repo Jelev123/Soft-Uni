bdbbfbdaccded16097618b8861440ef56ad45db8
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-02 13:39:40 +0300

    initial commit

---
+++ Advanced/Exercise-Defining Classes/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace DefiningClasses
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Kinder";
+            person.Age = 2;
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+
+        }
+        
+    }
+}
