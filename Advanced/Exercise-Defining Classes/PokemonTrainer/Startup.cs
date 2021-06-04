802447e2ae182cc1c542ea610c699cdb1b930d97
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-04 16:12:57 +0300

    initial commit

--- Advanced/Exercise-Defining Classes/PokemonTrainer/Startup.cs
+++ Advanced/Exercise-Defining Classes/PokemonTrainer/Startup.cs
@@ -10,7 +10,7 @@ namespace PokemonTrainer
         {
             string input = Console.ReadLine();
 
-            var trainers = new Dictionary<string, Trainers>();
+            var colectionOfTrainers = new Dictionary<string, Trainers>();
 
             while (input != "Tournament")
             {
@@ -22,15 +22,54 @@ namespace PokemonTrainer
                 string pokemonElement = info[2];
                 int pokemonHealth = int.Parse(info[3]);
 
-                if (!trainers.ContainsKey(trainerName))
+                if (!colectionOfTrainers.ContainsKey(trainerName))
                 {
-                    trainers.Add(trainerName,new Trainers(trainerName));
+                    colectionOfTrainers.Add(trainerName,new Trainers(trainerName));
                 }
 
-                Trainers currnetTrainer = trainers.FirstOrDefault(x => x.Key == trainerName).Value;
+                Trainers currnetTrainer = colectionOfTrainers.FirstOrDefault(x => x.Key == trainerName).Value;
                 Pokemon pokemon = new Pokemon(trainerName, pokemonName, pokemonElement, pokemonHealth);
 
+                currnetTrainer.CollectionOfPokemon.Add(pokemon);
+
                 input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string element = input;
+
+                foreach (var trainer in colectionOfTrainers)   
+                {
+                    if (trainer.Value.CollectionOfPokemon.Any(x=>x.Element==element))
+                    {
+                        trainer.Value.NumberOfBadges++;
+                    }
+                    else
+                    {
+                        foreach (var pokemon in trainer.Value.CollectionOfPokemon)
+                        {
+                            pokemon.Health -= 10;
+                        }
+
+                        trainer.Value.CollectionOfPokemon.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            var result = colectionOfTrainers.OrderByDescending(x => x.Value.NumberOfBadges);
+
+            foreach (var item in result)
+            {
+
+                Console.WriteLine($"{item.Key} {item.Value.NumberOfBadges} {item.Value.CollectionOfPokemon.Count}");
+
+
             }
         }
     }
