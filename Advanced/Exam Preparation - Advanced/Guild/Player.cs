c109a2b595ba215b1ea0a13e87b4152b6b057572
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-07 16:32:01 +0300

    initial commit

---
+++ Advanced/Exam Preparation - Advanced/Guild/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string clas)
+        {
+            Name = name;
+            Class = clas;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+        public override string ToString()
+        {
+            string output = $"Player {Name}: {Class}" + Environment.NewLine;
+            output += $"Rank: {Rank}" + Environment.NewLine;
+            output += $"Description: {Description}";
+            return output.TrimEnd();
+            //var sb = new StringBuilder();
+            //sb.AppendLine($"Players in the guild: Player {Name}: {Class}")
+            //    .AppendLine($"Rank: {Rank}")
+            //    .AppendLine($"Description: {Description}");
+
+            //return sb.ToString().TrimEnd();
+        }
+    }
+}
