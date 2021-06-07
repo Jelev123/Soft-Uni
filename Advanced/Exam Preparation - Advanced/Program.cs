c4326222acce8608ea67269b52e5f4b669025981
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-07 16:30:05 +0300

    initial commit

---
+++ Advanced/Exam Preparation - Advanced/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+               var firstBox = new Queue<int>(Console.ReadLine().Split()
+                .Select(int.Parse).ToList());
+
+               Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split()
+                   .Select(int.Parse).ToList());
+
+               var claimed = new List<int>();
+
+               while (firstBox.Count > 0 && secondBox.Count > 0 )
+               {
+                   int box1 = firstBox.Peek();
+                   int box2 = secondBox.Peek();
+                   int sum = box1 + box2;
+                   
+                   if (sum % 2 == 0)
+                   {
+                       claimed.Add(firstBox.Dequeue() + secondBox.Pop());
+                   }
+                   else
+                   {
+                       firstBox.Enqueue(secondBox.Pop());
+                   }
+
+
+                   if (firstBox.Count <= 0)
+                   {
+                       Console.WriteLine("First lootbox is empty");
+                   }
+                   else if (secondBox.Count <= 0)
+                   {
+                       Console.WriteLine("Second lootbox is empty");
+                   }
+
+            }
+
+               if (claimed.Sum() >= 100)
+               {
+                   Console.WriteLine($"Your loot was epic! Value: {claimed.Sum()}");
+               }
+               else
+               {
+                   Console.WriteLine($"Your loot was poor... Value: {claimed.Sum()}");
+               }
+               
+
+
+        }
+
+
+    }
+    }
+
