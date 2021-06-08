bc8c4b8401f289901a3325bb9ec368961ab33bce
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-08 14:09:23 +0300

    initial commit

---
+++ Advanced/!!EXAM PREPARATION!!/Parking/Car.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Parking
+{
+    public class Car
+    {
+        public Car(string manufacturer, string model, int year)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Year = year;
+        }
+
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Manufacturer} {this.Model} ({this.Year})");
+            return sb.ToString().TrimEnd();
+
+
+        }
+
+#endregion
+    }
+}
