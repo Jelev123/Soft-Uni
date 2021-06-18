6588b59fe37ec405bc972f76cc41e804c9498fca
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-18 18:44:46 +0300

    initial commit

---
+++ Advanced/!!EXAM PREPARATION!!/C# Advanced Retake Exam - 16 December 2020/Opening/Bakery.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+
+    {
+        private Dictionary<string, Employee> employees = new Dictionary<string, Employee>();
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            
+        }
+
+       
+        public string Name { get; set; }        
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return employees.Count;
+            }
+        }
+
+
+        public  void Add(Employee employee)
+        {
+
+            if (employees.Count < Capacity)
+            {
+                employees.Add(employee.Name,employee);
+            }
+            
+        }
+
+        public bool Remove(string name)
+        {
+            if (employees.ContainsKey(name))
+            {
+                employees.Remove(name);
+                return true;
+            }
+            return false;
+
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            Employee oldestEmplyee = null;
+
+            foreach (var employee in employees)
+            {
+                oldestEmplyee = new Employee(employee.Value.Name, employee.Value.Age, employee.Value.Country);
+                break;
+            }
+
+            return oldestEmplyee;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = null;
+
+            if (employees.ContainsKey(name))
+            {
+                employee = employees[name];
+                return employee;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"Employee: {employee.Value.Name}, {employee.Value.Age} ({employee.Value.Country})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ Advanced/!!EXAM PREPARATION!!/C# Advanced Retake Exam - 16 December 2020/Opening/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+            return $"Employee: {this.Name}, {this.Age} ({this.Country})";
+        }
+
+        #endregion
+    }
+}
