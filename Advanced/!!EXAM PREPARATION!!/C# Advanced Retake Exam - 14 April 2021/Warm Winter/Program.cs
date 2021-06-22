c8038919d6b415b5723ccb2a2f88fda5fedf0e6c
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-22 17:33:11 +0300

    initial commit

---
+++ Advanced/!!EXAM PREPARATION!!/C# Advanced Retake Exam - 14 April 2021/Warm Winter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Warm_Winter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackHat = new Stack<int>(Console.ReadLine().Split()
+                .Select(int.Parse)
+                .ToArray());
+            var queeueScarf = new Queue<int>(Console.ReadLine().Split()
+                .Select(int.Parse).ToArray());
+
+            int sets = int.MinValue;
+            var clothes = new List<int>();
+
+            while (stackHat.Count > 0 && queeueScarf.Count > 0)
+            {
+                if (stackHat.Peek() > queeueScarf.Peek())
+                {
+                    clothes.Add(stackHat.Peek() + queeueScarf.Peek());
+                    stackHat.Pop();
+                    queeueScarf.Dequeue();
+
+                }
+                else if (queeueScarf.Peek() > stackHat.Peek())
+                {
+                    stackHat.Pop();
+                }
+                else if (stackHat.Peek() == queeueScarf.Peek())
+                {
+                    queeueScarf.Dequeue();
+                    stackHat.Push(stackHat.Pop() + 1);
+
+                }
+
+
+            }
+            Console.WriteLine($"The most expensive set is: {clothes.Max()}");
+
+            Console.WriteLine(string.Join(" ",clothes));
+
+
+        }
+    }
+}
