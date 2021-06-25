0acea5f494d5d55a2b8a52a91df3f6a14183158e
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-25 16:32:07 +0300

    initial commit

---
+++ Advanced/!!EXAM PREPARATION!!/C# Advanced Exam - 25 October 2020/Garden/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimension = Console.ReadLine().Split();
+            int rows = int.Parse(dimension[0]);
+            int cols = int.Parse(dimension[1]);
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Bloom Bloom Plow")
+                {
+                    break;
+                }
+
+                var rowIndex = int.Parse(command[0].ToString());
+                var colIndex = int.Parse(command[2].ToString());
+
+                if (rowIndex < 0 || rowIndex > matrix.GetLength(0) && colIndex < 0 || colIndex > matrix.GetLength(1))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+                
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        matrix[rowIndex, i]++;
+                    }
+
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[j, colIndex]++;
+                    }
+
+                    matrix[rowIndex, colIndex]--;
+
+                 
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col     = 0; col     < cols; col    ++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
