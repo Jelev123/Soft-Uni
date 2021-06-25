0077e181c854de97f8144ae1e89d09afce7a3dce
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-25 15:31:11 +0300

    initial commit

---
+++ Advanced/!!EXAM PREPARATION!!/C# Advanced Exam - 25 October 2020/CPU/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CPU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackTask = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            var queueThreads = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            int valueToBeKilled = int.Parse(Console.ReadLine());
+
+            bool isKilled = false;
+
+
+            while (true)
+            {
+
+                if (stackTask.Peek() == valueToBeKilled)
+                {
+                    break;
+                }
+                if (queueThreads.Peek() >= stackTask.Peek())
+                {
+                    queueThreads.Dequeue();
+                    stackTask.Pop();
+
+                }
+                else if (queueThreads.Peek() < stackTask.Peek())
+                {
+                    queueThreads.Dequeue();
+                   
+                   
+
+                }
+               
+            }
+
+            Console.WriteLine($"Thread with value {queueThreads.Peek()} killed task {valueToBeKilled}");
+            Console.WriteLine(string.Join(" ",queueThreads));
+
+            
+
+        }
+    }
+}
