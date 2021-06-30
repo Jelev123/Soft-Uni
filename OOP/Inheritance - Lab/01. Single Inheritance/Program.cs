bac40905f77ad50a81e645620f9cbd4acf3e60bf
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-30 12:18:32 +0300

    initial commit

---
+++ OOP/Inheritance - Lab/01. Single Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Single_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
