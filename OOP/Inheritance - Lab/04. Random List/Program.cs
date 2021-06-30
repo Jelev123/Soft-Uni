97bc3d891ad2cf6385181f58a0de0942962a6e27
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-30 17:10:49 +0300

    initial commit

---
+++ OOP/Inheritance - Lab/04. Random List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList random = new RandomList();
+            random.Add("kinder");
+
+            Console.WriteLine(random.Count);
+
+            Console.WriteLine(random.RandomString());
+        }
+    }
+}
