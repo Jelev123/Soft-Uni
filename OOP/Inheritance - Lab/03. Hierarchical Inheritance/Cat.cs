9b16cdd7a66e2bedac79a841a0300f07d40761c8
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-06-30 16:56:26 +0300

    initial commit

---
+++ OOP/Inheritance - Lab/03. Hierarchical Inheritance/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public void Meowing()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
---
+++ OOP/Inheritance - Lab/03. Hierarchical Inheritance/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
