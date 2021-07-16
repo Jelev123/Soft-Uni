de6708d9f41179f75df0a303bddc5a23918ca365
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-07-16 19:53:52 +0300

    initial commit

---
+++ OOP/Interfaces and Abstraction - Exercise/Interfaces and Abstraction - Exercise/03. Telephony/Exceptions/InvalidNumberException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._Telephony.Exceptions
+{
+    class InvalidNumberException : Exception
+    {
+        private const string NUM_EXC_MSG = "Invalid number!";
+
+        public InvalidNumberException()
+            : base(NUM_EXC_MSG)
+        {
+            
+        }
+    }
+}
---
+++ OOP/Interfaces and Abstraction - Exercise/Interfaces and Abstraction - Exercise/03. Telephony/IBrowseble.cs
@@ -0,0 +1,8 @@
+﻿namespace _03._Telephony
+{
+    public interface IBrowseble
+    {
+        string Browse(string url);
+
+    }
+}
