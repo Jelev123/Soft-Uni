a781a57bc257463639f57edee7cabf4e9fedd6ea
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-08-07 18:07:39 +0300

    initial commit

---
+++ OOP/EXAM_PREP!!/C# OOP Exam - 12 December 2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_OOP_Exam___12_December_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
