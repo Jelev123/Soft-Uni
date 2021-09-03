dac01d1e8b3a61761079db8fe85d83c7a4d8d7ef
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-09-03 19:28:34 +0300

    initial commit

--- DB Basics/Table Relations/Table Relations.sql
+++ DB Basics/Table Relations/Table Relations.sql
@@ -1,23 +1,34 @@
 CREATE TABLE Passports(
-PassportID int PRIMARY KEY NOT null,
+PassportID int PRIMARY KEY IDENTITY	 NOT null,
 PassportNumber nvarchar (30) NOT null)
-
-
-
 
 CREATE TABLE Persons( 
 PersonID int PRIMARY KEY NOT null,
 FirstName nvarchar(	10) NOT null,
-Salary decimal NOT NULL ,
-PassportID int FOREIGN	KEY REFERENCES dbo.Passports (PassportID))
+Salary decimal (7,2) NOT NULL ,
+PassportID int  not null FOREIGN 	KEY REFERENCES dbo.Passports (PassportID) UNIQUE	)
 
+
+SET IDENTITY_INSERT	dbo.Passports	ON	
+
+INSERT	INTO		dbo.Passports
+(
+    PassportID,
+    PassportNumber
+)
+VALUES
+
+(101,'N34FG21B'),
+(102,'K65LO4R7'),
+(103,'ZE657QP2')
+    
 
 
 
 INSERT INTO dbo.Persons
 (
     PersonID,
-    FirstName,
+    [FirstName],
     Salary,
     PassportID
 )
@@ -29,4 +40,5 @@ VALUES
     
      
 	 SELECT * FROM dbo.Persons p	
+	 SELECT * FROM dbo.Passports p	
    
