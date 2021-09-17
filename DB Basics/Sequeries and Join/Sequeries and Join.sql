0c5753ccaece5bd97b97df06e1fc79131665ff36
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-09-17 14:39:52 +0300

    initial commit

--- DB Basics/Sequeries and Join/Sequeries and Join.sql
+++ DB Basics/Sequeries and Join/Sequeries and Join.sql
@@ -2,3 +2,10 @@ select top(5) EmployeeId,JobTitle,a.AddressID,AddressText from Employees as e
 join Addresses as a 
 on e.AddressId = a.AddressId
 order by AddressId asc
+
+select top(50) FirstName,LastName,t.[Name],AddressText from Employees as e
+join Addresses as a
+on e.AddressID = a.AddressId
+join Towns as t
+on a.TownID = t.TownID
+order by FirstName asc,LastName asc  
