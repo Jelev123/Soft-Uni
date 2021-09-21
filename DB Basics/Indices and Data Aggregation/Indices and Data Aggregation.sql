90710086d47f1d58a9d54ef0b36558dc722de569
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-09-21 18:18:36 +0300

    initial commit

---
+++ DB Basics/Indices and Data Aggregation/Indices and Data Aggregation.sql
@@ -0,0 +1,13 @@
+--03. Longest Magic Wand per Deposit Groups
+select DepositGroup,Max(MagicWandSize) as LongestMagicWand from WizzardDeposits
+group by DepositGroup
+
+--04. Smallest Deposit Group per Magic Wand Size
+
+select top(2) DepositGroup  from WizzardDeposits
+group by  DepositGroup
+order by Avg(MagicWandSize)
+
+-- 05. Deposits Sum
+
+select * from WizzardDeposits
