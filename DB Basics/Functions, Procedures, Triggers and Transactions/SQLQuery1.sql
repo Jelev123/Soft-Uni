b7d4068620bec55b72736d93e5146a8f650ce9ea
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-09-27 12:53:25 +0300

    initial commit

--- DB Basics/Functions, Procedures, Triggers and Transactions/SQLQuery1.sql
+++ DB Basics/Functions, Procedures, Triggers and Transactions/SQLQuery1.sql
@@ -4,3 +4,14 @@ create procedure usp_GetEmployeesSalaryAbove35000 as
 select FirstName,LastName
 from Employees
 where Salary > 35000
+
+
+-- 02. Employees with Salary Above Number
+
+create procedure usp_GetEmployeesSalaryAboveNumber
+(@Salary decimal= 18)
+as
+select FirstName,LastName
+from Employees
+where Salary >= @Salary
+
