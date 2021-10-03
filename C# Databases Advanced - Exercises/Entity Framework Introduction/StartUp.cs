f81c2a660fed581616daaa6e8d17346aead2cdd4
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-10-03 15:42:24 +0300

    initial commit

--- C# Databases Advanced - Exercises/Entity Framework Introduction/StartUp.cs
+++ C# Databases Advanced - Exercises/Entity Framework Introduction/StartUp.cs
@@ -5,12 +5,12 @@ using SoftUni.Data;
 
 namespace SoftUni
 {
-  public class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
             SoftUniContext softUniContext = new SoftUniContext();
-            string result = GetEmployeesFullInformation(softUniContext);
+            string result = GetEmployeesWithSalaryOver50000(softUniContext);
             Console.WriteLine(result);
 
         }
@@ -46,7 +46,30 @@ namespace SoftUni
 
         // 04. Employees with Salary Over 50 000
 
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                }).OrderBy(e => e.FirstName).ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
 
 
     }
 }
+
+
+
