1e0607adaf321e21b32f3ecb1bfcfd7814c6b3df
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-10-03 15:30:09 +0300

    initial commit

---
+++ C# Databases Advanced - Exercises/Entity Framework Introduction/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+  public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string result = GetEmployeesFullInformation(softUniContext);
+            Console.WriteLine(result);
+
+        }
+
+        // 03. Employees Full Information
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employyes = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                    e.EmployeeId
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            foreach (var employye in employyes)
+            {
+                sb.AppendLine(
+                    $"{employye.FirstName} {employye.LastName} {employye.MiddleName} {employye.JobTitle} {employye.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
