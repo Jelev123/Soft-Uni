f159bb90dcd04893bb8f22108ace382be9e29369
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-10-23 14:33:02 +0300

    Initial commit

---
+++ C# Databases Advanced - Exercises/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            var input = Console.ReadLine();
+
+            var result = GetBooksByAgeRestriction(db, input);
+
+            Console.WriteLine(result);
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var book = context.Books
+                .AsEnumerable()
+                .Where(s => s.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(s=>s.Title)
+                .OrderBy(s => s)
+                .ToList();
+
+            return String.Join(Environment.NewLine, book);
+        }
+    }
+}
