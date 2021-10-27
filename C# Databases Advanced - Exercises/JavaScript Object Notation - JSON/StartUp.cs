022a6d460342e1dc8a7e4bc2d31c0c6641a41016
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-10-27 14:41:12 +0300

    initial commit

---
+++ C# Databases Advanced - Exercises/JavaScript Object Notation - JSON/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext db = new ProductShopContext();
+
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+
+            string result = ImportUsers(db, inputJson);
+            Console.WriteLine(result);
+        }
+
+
+        private static void ResetDataBase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Successfuly Deleted!");
+            db.Database.EnsureCreated();
+            Console.WriteLine("Successfuly Created");
+        }
+
+
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+
+        }
+    }
+}
