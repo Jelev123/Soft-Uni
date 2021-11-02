ed9626256aaa83350aef9feca067557a74a8133f
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-11-02 13:44:53 +0200

    ,

---
+++ C# Databases Advanced - Exercises/Extensible Markup Language - XML/ProductShop/Dtos/Import/ImportUsersDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType("User")]
+  public  class ImportUsersDto
+    {
+        [XmlAnyElement("firstname")]
+        public string FirstName { get; set; }
+        [XmlAnyElement("lastname")]
+        public string LastName { get; set; }
+        [XmlAnyElement("age")]
+        public int Age { get; set; }
+    }
+}
