6da93ad3f9512a6d278d9311708df06406814c8e
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2021-11-02 15:27:03 +0200

    initial commit

--- C# Databases Advanced - Exercises/Extensible Markup Language - XML/ProductShop/ProductShop/StartUp.cs
+++ C# Databases Advanced - Exercises/Extensible Markup Language - XML/ProductShop/ProductShop/StartUp.cs
@@ -1,4 +1,12 @@
-﻿using ProductShop.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using ProductShop.XMLHelper;
 
 namespace ProductShop
 {
@@ -8,10 +16,39 @@ namespace ProductShop
         {
             ProductShopContext db = new ProductShopContext();
 
-            
+            var xml = File.ReadAllText("../../../Datasets/users.xml");
+
+            var result = ImportUsers(db, xml);
+
+            Console.WriteLine(result);
+
         }
 
 
-        
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            const string rootElement = "Users";
+
+            var resultXml = XMLConverter.XmlConverter.Deserializer<ImportUsersDTO>(inputXml, rootElement);
+
+            var users = resultXml
+                .Select(x => new User()
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Age = x.Age
+                })
+                .ToArray();
+
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Length}";
+
+        }
+
+
+
+
     }
 }
